250d58b0947d905bc4e309172732090d9a8f6102
mhavelant <mhavelant@brainsum.com>
2020-12-10 15:09:33 +0100

    Lint the chart before packaging.

--- helm-package-and-index.sh
+++ helm-package-and-index.sh
@@ -7,6 +7,8 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 
 CHART=drupal
 
+helm lint --strict --debug "${SCRIPT_DIR}/charts/${CHART}" || exit 1
+
 helm package "${SCRIPT_DIR}/charts/${CHART}" -d "${SCRIPT_DIR}/docs"
 
 helm repo index docs --url https://brainsum.github.io/helm/
