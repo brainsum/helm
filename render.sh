c295c937aacce987837207abbb99d645b4277bf9
mhavelant <mhavelant@brainsum.com>
2021-01-22 17:15:35 +0100

    Update render script for tika-server.

--- render.sh
+++ render.sh
@@ -8,20 +8,35 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 
 
 PROJECT=helmdemo
-CHART="${SCRIPT_DIR}/charts/drupal"
+CHART="drupal"
+CHART_PATH="${SCRIPT_DIR}/charts/${CHART}"
 
 ENVIRONMENTS='staging production'
 
 for ENVIRONMENT in ${ENVIRONMENTS}
 do
-  VALUE_FILE="${SCRIPT_DIR}/tmp/values/${ENVIRONMENT}/values.yaml"
-  OUTPUT_DIR="${SCRIPT_DIR}/rendered/${ENVIRONMENT}"
+  VALUE_FILE="${SCRIPT_DIR}/tmp/values/${CHART}/${ENVIRONMENT}/values.yaml"
+  OUTPUT_DIR="${SCRIPT_DIR}/rendered/${CHART}/${ENVIRONMENT}"
   RELEASE_NAME="${PROJECT}-${ENVIRONMENT}"
 
   echo "Rendering to ${OUTPUT_DIR}"
   [ -d "${OUTPUT_DIR}" ] && rm -r "${OUTPUT_DIR}"
-  helm template "${RELEASE_NAME}" "${CHART}" \
+  helm template "${RELEASE_NAME}" "${CHART_PATH}" \
     -f "${VALUE_FILE}" \
     --output-dir "${OUTPUT_DIR}" \
     --debug
 done
+
+
+# ----------------------- #
+CHART="tika-server"
+CHART_PATH="${SCRIPT_DIR}/charts/${CHART}"
+OUTPUT_DIR="${SCRIPT_DIR}/rendered/${CHART}"
+VALUE_FILE="${SCRIPT_DIR}/tmp/values/${CHART}/values.yaml"
+OUTPUT_DIR="${SCRIPT_DIR}/rendered/${CHART}"
+RELEASE_NAME="${PROJECT}"
+
+#     -f "${VALUE_FILE}" \
+helm template "${RELEASE_NAME}" "${CHART_PATH}" \
+    --output-dir "${OUTPUT_DIR}" \
+    --debug
